7a6d7a3fda02fc4e6726fe0e6be05ad5868292c8
ZafCode14 <zafarwp14@gmail.com>
2024-04-10 01:07:03 +0200

    0

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,25 +1,18 @@
 #!/usr/bin/env python3
-"""This module contains a python script"""
 from typing import List
 
 
 def pascal_triangle(n):
-    """This function creates the pascal trinagle"""
+    """Pascal triangle"""
     if n <= 0:
         return []
-    elif n == 1:
-        return [[1]]
-    elif n == 2:
-        return [[1], [1, 1]]
 
-    triangle = [[1], [1, 1]]
-
-    for i in range(2, n):
-        temp = [1, 1]
-        for j in range(0, len(triangle[-1])-1):
-            a = triangle[-1][j]
-            b = triangle[-1][j+1]
-            temp.insert(-1, a + b)
-        triangle.append(temp)
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
 
     return triangle
