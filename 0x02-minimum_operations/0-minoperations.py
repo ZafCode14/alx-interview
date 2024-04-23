295900cdb4b077efc4d0e0d3810db27be186acd2
ZafCode14 <zafarwp14@gmail.com>
2024-04-23 12:43:39 +0200

    0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,20 +4,11 @@
 
 def minOperations(n):
     """Function that calculates the number of operations needed"""
-    if n <= 1:
-        return n
-
-    operations = 0
-    copied_chars = 1
-    buffer = 1
-
-    while copied_chars < n:
-        if n % copied_chars == 0:
-            operations += 2
-            buffer = copied_chars
-            copied_chars += buffer
-        else:
-            operations += 1
-            copied_chars += buffer
-
-    return operations
+    nOpe = 0
+    minOpe = 2
+    while n > 1:
+        while n % minOpe == 0:
+            nOpe += minOpe
+            n /= minOpe
+        minOpe += 1
+    return nOpe
