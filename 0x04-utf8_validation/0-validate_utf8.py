837308f77439ae14d520d31e385cad910f65968a
ZafCode14 <zafarwp14@gmail.com>
2024-05-07 14:41:00 +0300

    0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,34 +3,29 @@
 
 
 def validUTF8(data):
-    # Count of remaining bytes for the current character
-    remaining_bytes = 0
+    expected_bytes = 0
 
     for byte in data:
-        # Check if this byte is the start of a new character
-        if remaining_bytes == 0:
-            if byte >> 3 == 0b11110:  # 4-byte character
-                remaining_bytes = 3
-            elif byte >> 4 == 0b1110:  # 3-byte character
-                remaining_bytes = 2
-            elif byte >> 5 == 0b110:  # 2-byte character
-                remaining_bytes = 1
-            elif byte >> 7 == 0:  # 1-byte character
-                remaining_bytes = 0
+
+        if expected_bytes == 0:
+            if byte & 0b10000000 == 0:
+                expected_bytes = 0
+            elif byte & 0b11100000 == 0b11000000:
+                expected_bytes = 1
+            elif byte & 0b11110000 == 0b11100000:
+                expected_bytes = 2
+            elif byte & 0b11111000 == 0b11110000:
+                expected_bytes = 3
             else:
-                return False  # Invalid starting byte
+                return False
 
         else:
-            # Check if this byte is a continuation byte
-            if byte >> 6 != 0b10:
-                return False  # Invalid continuation byte
-            remaining_bytes -= 1
+            if byte & 0b11000000 != 0b10000000:
+                return False
 
-        # If there are negative remaining bytes,
-        # it means there are too many continuation bytes
-        if remaining_bytes < 0:
-            return False
+            expected_bytes -= 1
 
-    # If there are remaining bytes after iterating through all data,
-    # it means the data is incomplete
-    return remaining_bytes == 0
+            if expected_bytes == 0:
+                expected_bytes = 0
+
+    return expected_bytes == 0
