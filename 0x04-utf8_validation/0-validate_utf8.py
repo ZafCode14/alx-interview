ab89abd84edddd8c149814ea98d9f98d39465834
ZafCode14 <zafarwp14@gmail.com>
2024-05-07 14:37:48 +0300

    pycodestyle

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,7 +5,7 @@
 def validUTF8(data):
     # Count of remaining bytes for the current character
     remaining_bytes = 0
-    
+
     for byte in data:
         # Check if this byte is the start of a new character
         if remaining_bytes == 0:
@@ -19,16 +19,18 @@ def validUTF8(data):
                 remaining_bytes = 0
             else:
                 return False  # Invalid starting byte
-        
+
         else:
             # Check if this byte is a continuation byte
             if byte >> 6 != 0b10:
                 return False  # Invalid continuation byte
             remaining_bytes -= 1
-        
-        # If there are negative remaining bytes, it means there are too many continuation bytes
+
+        # If there are negative remaining bytes,
+        # it means there are too many continuation bytes
         if remaining_bytes < 0:
             return False
-    
-    # If there are remaining bytes after iterating through all data, it means the data is incomplete
+
+    # If there are remaining bytes after iterating through all data,
+    # it means the data is incomplete
     return remaining_bytes == 0
